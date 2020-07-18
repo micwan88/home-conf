6598cc759fe2c233e854f892e73a9caea208e754
micmic <hkmicmic@gmail.com>
2020-07-19 01:15:32 +0800

    enable chromedriver logging in run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,4 @@ cd ${APPDIR}
 
 set -o pipefail
 
-/usr/bin/java -cp "conf:libs/*" io.github.micwan88.homeconf.ConfDaemon | tee -a home-conf.log
+/usr/bin/java -Dwebdriver.chrome.logfile=`pwd`/chromelog.txt -Dwebdriver.chrome.verboseLogging=true -cp "conf:libs/*" io.github.micwan88.homeconf.ConfDaemon | tee -a home-conf.log
